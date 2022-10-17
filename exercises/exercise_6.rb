376d52d2f918cdfbd798528bdb7bed11671aac7d
Cindy <cindy.c0106@gmail.com>
2022-10-17 21:52:04 +0000

    adding code for exercise 6 and changed exercise 2 to find_by instead of where

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,11 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Korben", last_name: "Rennie", hourly_rate: 50)
+@store1.employees.create(first_name: "Rhea", last_name: "Cuevas", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Menna", last_name: "Woodward", hourly_rate: 60)
+@store2.employees.create(first_name: "Bryce", last_name: "Chambers", hourly_rate: 50)
+@store2.employees.create(first_name: "Selin", last_name: "Calvert", hourly_rate: 40)
+
